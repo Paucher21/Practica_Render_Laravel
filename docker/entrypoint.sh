88afd0690c2d1a04fb70c9c7f5713a56331542ba
paucher1207 <126487110+Paucher21@users.noreply.github.com>
2026-01-13 20:22:31 +0100

    condicional seeder

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,8 +8,12 @@ sleep 10
 echo "Ejecutando migraciones..."
 php artisan migrate --force
 
-echo "Ejecutando seeders..."
-php artisan db:seed --force
+if [ "$APP_ENV" != "production" ]; then
+    echo "Ejecutando seeders..."
+    php artisan db:seed --force
+else
+    echo "Entorno de producción detectado: Saltando seeders automáticos."
+fi
 
 echo "Iniciando servidor..."
 
